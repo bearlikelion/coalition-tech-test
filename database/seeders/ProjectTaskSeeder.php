5052ffdc6ed2de5b09303ff1b1cb4a6491fcf7d8
Mark <markarneman@gmail.com>
2025-11-16 16:06:37 -0500

    PT Seeder - Ignore database from git

---
+++ database/seeders/ProjectTaskSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Task;
+use Illuminate\Database\Seeder;
+
+class ProjectTaskSeeder extends Seeder
+{    
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = \Faker\Factory::create();
+        $projects = [
+            'Personal',
+            'Work',
+            'Open Source',
+        ];
+
+        foreach ($projects as $projectName) {
+            $project = Project::create(['name' => $projectName]);
+
+            for ($i = 1; $i <= 5; $i++) {
+                // Fake task creation
+                Task::create([
+                    'project_id' => $project->id,
+                    'name' => $faker->sentence(4),
+                    'priority' => $i,
+                ]);
+            }
+        }
+    }
+}
